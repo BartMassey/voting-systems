03c7930c9a81126811aaccf2a6e2e24257fb3e0f
Bart Massey <bart@cs.pdx.edu>
2018-09-19 02:13:24 -0700

    finished plurality voting; passes simple test

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 
 /// Newtype for candidates. For convenience, the candidates
 /// are assumed to be numbered contiguously starting at 0.
+#[derive(Debug, PartialEq, Eq)]
 pub struct Candidate(usize);
 
 /// Type for rankings, especially ballots.
@@ -24,6 +25,7 @@ impl Poll {
             .flat_map(|r| r.iter().map(|c| c.0))
             .max()
             .expect("empty poll")
+            + 1
     }
 }
 
@@ -31,16 +33,50 @@ impl Poll {
 pub trait VotingSystem {
 
     /// Run an election.
-    fn election(&Poll) -> Ranking;
+    fn election(Poll) -> Ranking;
 }
 
 /// Plurality voting ranks candidates by
 /// number of top ballot rankings.
-struct PluralityVoting;
+pub struct PluralityVoting;
 
 impl VotingSystem for PluralityVoting {
 
-    fn election(poll: &Poll) -> Ranking {
-        unimplemented!("count top rankings")
+    fn election(poll: Poll) -> Ranking {
+        let ncandidates = poll.ncandidates();
+        let mut votes = vec![0; ncandidates];
+        for r in poll.0 {
+            if r.is_empty() {
+                continue;
+            }
+            votes[r[0].0] += 1;
+        }
+        let mut ranking: Vec<Candidate> =
+            (0..ncandidates).map(|c| Candidate(c)).collect();
+        ranking.sort_by_key(|c| -votes[c.0]);
+        ranking
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST_POLL: &[&[usize]] = &[
+        &[2,1,0],
+        &[0,1,2],
+        &[0,2,1],
+        &[1,2],
+        &[],
+    ];
+
+    fn make_poll() -> Poll {
+        Poll(TEST_POLL.iter().map(|r| r.iter().map(|c| Candidate(*c)).collect()).collect())
+    }
+
+    #[test]
+    fn try_plurality() {
+        let result = PluralityVoting::election(make_poll());
+        assert_eq!(result[0], Candidate(0));
     }
 }
